ae0f32c089616a085ab9ec3e05fde186602dea4d
AndreaMyshkn <a.trkvsk@hotmail.com>
2021-10-14 11:30:36 -0500

    first commit

---
+++ resources/views/example.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       
+
+        <title>Example</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/app.css">
+     
+    </head>
+    <body class="antialiased">
+       <h1> In caudam semper stat venenum</h1>
+    </body>
+</html>
